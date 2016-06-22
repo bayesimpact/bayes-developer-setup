ebdccf7a8bc35c5cbc5ad37df7ee7c182ecaa24b
Stephan Gabler <stephan.gabler@gmail.com>
2016-06-22 14:17:29 +0200

    Implement a custom reviewable rule

--- README.md
+++ README.md
@@ -12,3 +12,11 @@ Run tests for `git-submit` using Docker:
 
 * [Install](http://go/wiki/Docker) docker and docker-compose.
 * Run `docker-compose run --rm test`
+
+Run tests for the Reviewable rule using Docker:
+
+* Run `docker-compose run --rm test-reviewable`
+
+During development, you can have the tests be re-executed on any change you make
+to the files. In order to do that run the container with:
+`docker-compose run --rm test-reviewable npm run test:watch`.
---
+++ reviewable/lgtm_plus_resolved_discussions.js
@@ -0,0 +1,55 @@
+'use strict'
+// This code will check that the pull request has been approved
+// via LGTM (Looks Good To Me) emojis by at least one assignee.
+// Additionally the PR author has to answer all discussions.
+
+// When run on reviewable, this script will have a `review` object
+// accessible in its scope. This object contains all the information
+// about the current PR.
+
+/*global _ review:true*/
+
+const descriptions = []
+
+// LGTM approval.
+// TODO: Check for LGTM cancellation.
+// Approval by username
+const approvals = []
+_.each(review.sentiments, function(sentiment) {
+  const emojis = _.indexBy(sentiment.emojis)
+  if (emojis.lgtm) {
+    approvals.push(sentiment.username)
+  }
+})
+
+const assignees = _.pluck(review.pullRequest.assignees, 'username')
+const atLeastOneLgtm = !!_.intersection(approvals, assignees).length
+if (!atLeastOneLgtm) {
+  descriptions.push('LGTM missing')
+}
+
+
+// Discussion resolved approval.
+const author = review.pullRequest.author.username
+const discussionsBlockedByAuthor = _.chain(review.discussions).
+    pluck('participants').
+    flatten().
+    where({resolved: false, username: author, disposition: 'discussing'}).
+    value()
+const allDiscussionsResolved = !discussionsBlockedByAuthor.length
+if (!allDiscussionsResolved) {
+  descriptions.push('Unresolved discussions')
+}
+
+
+// Output
+const description = descriptions.join(', ')
+const shortDescription = description
+
+return {
+  completed: atLeastOneLgtm && allDiscussionsResolved,
+  pendingReviewers: [],
+  description,
+  shortDescription,
+  debug: {atLeastOneLgtm, allDiscussionsResolved},
+}
