33b66335408c5b8768e12953bba598cfbf24801a
Pascal Corpet <pascal@bayesimpact.org>
2018-03-26 08:21:56 +0200

    Accept LGTMs from rquested reviewers even if they are not assignees.

--- reviewable/lgtm_plus_resolved_discussions.js
+++ reviewable/lgtm_plus_resolved_discussions.js
@@ -1,6 +1,7 @@
 'use strict'
 // This code will check that the pull request has been approved
-// via LGTM (Looks Good To Me) emojis by at least one assignee.
+// via LGTM (Looks Good To Me) emojis by at least one assignee or requested
+// reviewer.
 // Additionally the PR author has to answer all discussions.
 
 // When run on reviewable, this script will have a `review` object
@@ -22,20 +23,24 @@ _.each(review.sentiments, function(sentiment) {
   }
 })
 
-const assignees = _.pluck(review.pullRequest.assignees, 'username')
-const atLeastOneLgtm = !!_.intersection(approvals, assignees).length
-if (!atLeastOneLgtm) {
-  descriptions.push('LGTM missing')
+const reviewers = _.union(
+  _.pluck(review.pullRequest.requestedReviewers, 'username'),
+  _.pluck(review.pullRequest.assignees, 'username'))
+const atLeastOneLgtm = !!_.intersection(approvals, reviewers).length
+if (!reviewers.length) {
+  descriptions.push('Missing an assignee or a reviewer')
+} else if (!atLeastOneLgtm) {
+  descriptions.push('LGTM missing from one of: ' + reviewers)
 }
 
 
 // Discussion resolved approval.
 const author = review.pullRequest.author.username
 const discussionsBlockedByAuthor = _.chain(review.discussions).
-    pluck('participants').
-    flatten().
-    where({resolved: false, username: author, disposition: 'discussing'}).
-    value()
+  pluck('participants').
+  flatten().
+  where({resolved: false, username: author, disposition: 'discussing'}).
+  value()
 const allDiscussionsResolved = !discussionsBlockedByAuthor.length
 if (!allDiscussionsResolved) {
   descriptions.push('Unresolved discussions')
